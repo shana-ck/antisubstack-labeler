5800778ba80ae0b9fcd1ed28dfcd2b02a3aff68e
shana-ck <s.klein1127@gmail.com>
2026-09-04 09:50:21 -0400

    remove unnecessary console logs

--- expandurl.ts
+++ expandurl.ts
@@ -1,20 +1,27 @@
-import fetch from 'node-fetch';
+import { Agent, fetch } from "undici";
 
 async function expandUrl(shortUrl) {
+    const controller = new AbortController();
+    const timeout = setTimeout(() => controller.abort(), 5000);
+    const pool = new Agent({ connect: { keepAlive: false}})
   try {
     // By default, fetch follows redirects ('follow').
     const response = await fetch(shortUrl, {
+      dispatcher: pool,
       method: 'HEAD',
-      redirect: 'follow'
+      redirect: 'follow',
+      signal: controller.signal
     });
     
-    // The final URL is available in response.url after all redirects are followed
-    console.log("Original URL:", response.url);
+
     return response.url;
 
   } catch (error) {
-    console.error("Error expanding URL:", error);
+
     return shortUrl
+  }
+  finally {
+   clearTimeout(timeout);
   }
 }
 
